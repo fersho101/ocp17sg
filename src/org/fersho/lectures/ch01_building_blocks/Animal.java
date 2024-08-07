ad2c6bd217235b91b94b2751bbeca17594e2bf64
fersho101 <corral.fdo@gmail.com>
2024-08-06 23:29:05 -0700

    Animal

---
+++ src/org/fersho/lectures/ch01_building_blocks/Animal.java
@@ -0,0 +1,13 @@
+package org.fersho.lectures.ch01_building_blocks;
+
+public class Animal {
+    String name;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        name = newName;
+    }
+}
+
