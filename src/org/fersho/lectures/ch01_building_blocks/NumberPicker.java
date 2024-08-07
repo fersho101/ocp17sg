59fc8a3a55f060a05b6ab99ab628494b46b2ad51
fersho101 <corral.fdo@gmail.com>
2024-08-07 00:13:47 -0700

    NumberPicker

---
+++ src/org/fersho/lectures/ch01_building_blocks/NumberPicker.java
@@ -0,0 +1,13 @@
+package org.fersho.lectures.ch01_building_blocks;
+
+import java.util.Random;
+
+public class NumberPicker {
+
+    public static void main(String[] ar) {
+
+        Random r = new Random(); 
+        System.out.println(r.nextInt(10));        
+    }
+
+}
