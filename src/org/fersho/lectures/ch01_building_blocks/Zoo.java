b85b924b3b265cb690119d40f788221c1e363134
fersho101 <corral.fdo@gmail.com>
2024-08-06 23:54:44 -0700

    javac path and java run

---
+++ src/org/fersho/lectures/ch01_building_blocks/Zoo.java
@@ -0,0 +1,13 @@
+package org.fersho.lectures.ch01_building_blocks;
+
+public class Zoo {
+    public static void main(String...args) {
+        System.out.println("Hola, Zoo");
+        System.out.println("Hola Girafa");
+
+        /*
+           PS C:\sg\ocp17sg\src> javac -d C:\sg\ocp17sg\bin\org\fersho\lectures\ch01_building_blocks\ org\fersho\lectures\ch01_building_blocks\Zoo.java
+           PS C:\sg\ocp17sg\src> java -cp C:\sg\ocp17sg\bin\org\fersho\lectures\ch01_building_blocks\ org.fersho.lectures.ch01_building_blocks.Zoo
+         */
+    }
+}
