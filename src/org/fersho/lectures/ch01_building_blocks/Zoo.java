899e1897a08634f845f99f74c6c94bd357c7c318
fersho101 <corral.fdo@gmail.com>
2024-08-07 00:05:52 -0700

    passing parameters

--- src/org/fersho/lectures/ch01_building_blocks/Zoo.java
+++ src/org/fersho/lectures/ch01_building_blocks/Zoo.java
@@ -4,6 +4,8 @@ public class Zoo {
     public static void main(String...args) {
         System.out.println("Hola, Zoo");
         System.out.println("Hola Girafa");
+        System.out.println(args[0]);
+        System.out.println(args[1]);
 
         /*
            PS C:\sg\ocp17sg\src> javac -d C:\sg\ocp17sg\bin\org\fersho\lectures\ch01_building_blocks\ org\fersho\lectures\ch01_building_blocks\Zoo.java
