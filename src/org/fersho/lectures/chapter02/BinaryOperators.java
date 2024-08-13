c4d099e5e925d5169675ac83affee59cd76261b3
fersho101 <corral.fdo@gmail.com>
2024-08-13 12:50:02 -0700

    .

---
+++ src/org/fersho/lectures/chapter02/BinaryOperators.java
@@ -0,0 +1,28 @@
+package org.fersho.lectures.chapter02;
+
+public class BinaryOperators {
+
+    public static void main(String[] args) {
+
+        /* ~Addition and Subtraction */
+        // int price = 2 * 5 + 3 * 4 - 8;
+        // System.out.println(price);
+        // price = 2 * ((5 + 3) * 4 - 8);
+        // System.out.println(price);
+
+        // Division and Modulus
+
+        // System.out.println(9 / 3);
+        // System.out.println(9 % 3);
+        // System.out.println(10 / 3);
+        // System.out.println(10 % 3);
+        // System.out.println(11 / 3);
+        // System.out.println(11 % 3);
+        // System.out.println(12 / 3);
+        // System.out.println(12 % 3);
+        // System.out.println(-12.5 % 5);
+
+
+    }
+
+}
