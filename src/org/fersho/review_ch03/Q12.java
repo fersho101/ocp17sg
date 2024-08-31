e7797858720bb237981ee8a1de8d99303547c3c1
fersho101 <corral.fdo@gmail.com>
2024-08-31 15:57:23 -0700

    Q12

--- src/org/fersho/review_ch03/Q12.java
+++ src/org/fersho/review_ch03/Q12.java
@@ -12,12 +12,12 @@ package org.fersho.review_ch03;
 
 public class Q12 {
     public static void main(String[] args) {
-        
+
         int sing = 8, squawk = 2, notes = 0;
-        while(sing > squawk) {
+        while (sing > squawk) {
             sing--;
             squawk += 2;
-            notes += sing + squawk; 
+            notes += sing + squawk;
         }
         System.out.println(notes);
     }
