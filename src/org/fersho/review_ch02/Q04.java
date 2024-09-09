a0e8534be32bff9d68be655e00f9e745b87b31be
fersho101 <corral.fdo@gmail.com>
2024-09-08 19:18:35 -0700

    Q04

--- src/org/fersho/review_ch02/Q04.java
+++ src/org/fersho/review_ch02/Q04.java
@@ -15,7 +15,8 @@ public class Q04 {
        boolean canine = true, wolf = true;
        int teeth = 20;
        canine = (teeth != 10) ^ (wolf = false);
-       System.out.println(canine + ", " + teeth + ", " + wolf);       
+       System.out.println(canine + ", " + teeth + ", " + wolf);        
+        
     }
 
 }
