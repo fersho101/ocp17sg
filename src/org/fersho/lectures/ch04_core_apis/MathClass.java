7864dc2cbc92e51bf308eb1815786c563cd2fe25
fersho101 <corral.fdo@gmail.com>
2024-09-23 11:27:17 -0700

    some Math methods

---
+++ src/org/fersho/lectures/ch04_core_apis/MathClass.java
@@ -0,0 +1,14 @@
+package org.fersho.lectures.ch04_core_apis;
+
+public class MathClass {
+    public static void main(String[] args) {
+        System.out.println(Math.min(3,6));
+        System.out.println(Math.max(3,6));
+        System.out.println(Math.round(3.6));
+        System.out.println(Math.round(3.4));
+        System.out.println(Math.ceil(3.4));
+        System.out.println(Math.floor(3.4));
+        System.out.println(Math.pow(3,2));
+        System.out.println(Math.random());
+    }
+}
