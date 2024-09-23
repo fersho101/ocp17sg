9fe81d38f699f26177ecb14527ad6e5e18e6b634
fersho101 <corral.fdo@gmail.com>
2024-09-23 09:51:54 -0700

    varargs

---
+++ src/org/fersho/lectures/ch04_core_apis/UsingVarargs.java
@@ -0,0 +1,14 @@
+package org.fersho.lectures.ch04_core_apis;
+
+public class UsingVarargs {
+
+    public static void main(String[] args) {
+        
+        System.out.println(args.length);
+        System.out.println(args[0]);
+        System.out.println(args[args.length - 1]);
+       
+
+    }
+
+}
