bcc320649bc5dfa4157a437bad35a62f5b4171c7
fernando corral <146652466+fersho101@users.noreply.github.com>
2024-10-19 12:04:44 -0700

    .

---
+++ src/org/fersho/lectures/ch06_class_design/declaring_subclass/Mammal.java
@@ -0,0 +1,5 @@
+package org.fersho.lectures.ch06_class_design.declaring_subclass;
+
+public class Mammal {
+
+}
---
+++ src/org/fersho/lectures/ch06_class_design/declaring_subclass/Rhinoceros.java
@@ -0,0 +1,7 @@
+package org.fersho.lectures.ch06_class_design.declaring_subclass;
+
+public final class Rhinoceros extends Mammal {
+
+}
+
+//class Clara extends Rhinoceros { } // Does not compile, Rhinoceros is market final
