c7ab219a1d25e6d2e2f9f310c1f34903c242879a
fernando corral <146652466+fersho101@users.noreply.github.com>
2024-10-19 18:32:49 -0700

    .

---
+++ src/org/fersho/lectures/ch06_class_design/class_access_mods/Bear.java
@@ -0,0 +1,16 @@
+package org.fersho.lectures.ch06_class_design.class_access_mods;
+
+class Bird {
+}
+
+class Bear {
+}
+
+class Fish {
+}
+
+
+
+// protected class ClownFish {} //Does not compile
+
+// private class BlueTang {} //Does not compile
