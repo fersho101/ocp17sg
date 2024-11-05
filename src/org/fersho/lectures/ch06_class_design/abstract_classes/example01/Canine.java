7d0171dd2a22d0c030adb8adb684a89e615a489c
fernando corral <146652466+fersho101@users.noreply.github.com>
2024-11-05 16:30:04 -0700

    .

--- src/org/fersho/lectures/ch06_class_design/abstract_classes/example01/Canine.java
+++ src/org/fersho/lectures/ch06_class_design/abstract_classes/example01/Canine.java
@@ -7,3 +7,33 @@ public abstract class Canine {
         System.out.println(getSound());
     }
 }
+
+// bad abstract implementations 
+
+// class FennecFox extends Canine {
+//     public int getSound() { // return type is incompatible
+//         return 10;
+//     }
+// }
+
+// class ArcticFox extends Canine {} // does not implement the inherit abstract method
+
+class DireWolf extends Canine {
+    // public abstract rest(); //  class isn't abstract and declare an abstract method.
+    public String getSound() {
+        return "Roof!";
+    }
+}
+
+class Jackal extends Canine {
+    // public abstract String name; // illegal modifier field
+    public String getSound() {
+        return "Laugh!";
+    }
+}
+
+abstract class Alligator {
+    public static void main(String[] args) {
+        // var a = new Alligator(); 
+    }
+}
