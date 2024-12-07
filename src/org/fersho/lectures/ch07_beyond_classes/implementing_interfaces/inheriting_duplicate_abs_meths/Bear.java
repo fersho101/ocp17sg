ca711368acd2d6670a3353ef7a518e8b94bf5810
fersho101 <146652466+fersho101@users.noreply.github.com>
2024-12-06 17:58:06 -0700

    .

---
+++ src/org/fersho/lectures/ch07_beyond_classes/implementing_interfaces/inheriting_duplicate_abs_meths/Bear.java
@@ -0,0 +1,35 @@
+package org.fersho.lectures.ch07_beyond_classes.implementing_interfaces.inheriting_duplicate_abs_meths;
+
+interface Herbivore {
+    public void eatPlants();
+}
+
+interface Omnivore {
+    public void eatPlants();
+}
+
+interface Carnivore {
+    public void eatMeat():
+}
+
+interface Carnivore2 {
+    public int eatMeat();
+}
+
+public class Bear implements Herbivore, Omnivore {
+
+    @Override
+    public void eatPlants() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'eatPlants'");
+    }
+
+}
+
+// class Tiger implements Carnivore, Carnivore2 {
+
+// // The return type is incompatible with Carnivore.eatMeat()
+
+// public int eatMeat() {}
+
+// }
