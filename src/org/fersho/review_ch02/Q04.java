e52c267d6fccd41179913f5d63856fec6a3162b8
fernando corral <146652466+fersho101@users.noreply.github.com>
2025-03-05 18:08:01 -0700

    .

--- src/org/fersho/review_ch02/Q04.java
+++ src/org/fersho/review_ch02/Q04.java
@@ -12,11 +12,11 @@ package org.fersho.review_ch02;
 
 public class Q04 {
     public static void main(String[] args) {
-       boolean canine = true, wolf = true;
-       int teeth = 20;
-       canine = (teeth != 10) ^ (wolf = false);
-       System.out.println(canine + ", " + teeth + ", " + wolf);        
-        
+        boolean canine = true, wolf = true;
+        int teeth = 20;
+        canine = (teeth != 10) ^ (wolf = false);
+        System.out.println(canine + ", " + teeth + ", " + wolf);
+
     }
 
 }
